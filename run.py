92da078bb7066d481f932b879dfb77ae6bbe0800
刘文道 <thinkingstill@users.noreply.github.com>
2024-12-31 01:25:59 +0800

    fix tqdm lost

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ import json
 import sys
 import os
 import time
+import tqdm
 
 TODAY_STR = time.strftime("%Y%m%d", time.localtime())
 
@@ -176,7 +177,7 @@ if __name__ == "__main__":
         for idx in tqdm.tqdm(range(stock_basic_info.shape[0])):
             code = stock_basic_info.loc[idx, '股票代码']
             try:
-                tmp_df = ak.stock_zh_a_hist(symbol=code, period="daily", start_date="20240923", end_date='20241230', adjust="")
+                tmp_df = ak.stock_zh_a_hist(symbol=code, period="daily", start_date="20240923", end_date=LATEST_EXCHAGE_DATE, adjust="")
             except Exception as e:
                 continue
             totla_stock_range.append(tmp_df)
